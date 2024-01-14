b09ac6af9a4e379e706fb4d33cf002814a2c7460
Akshat Jain <akshat00jain@gmail.com>
2024-01-14 14:58:51 +0530

    added role id command

---
+++ src/slashCommands/role_id.ts
@@ -0,0 +1,22 @@
+import { ChannelType, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../types";
+
+const ClearCommand : SlashCommand = {
+    command: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Delets messages from the current channel.")
+    .addRoleOption(option => {
+        return option
+        .setName("role")
+        .setDescription("Role to send id.")
+        .setRequired(true)
+    })
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    execute: interaction => {
+        const role = interaction.options.getRole("role")
+        interaction.reply(role!.id)
+    },
+    cooldown: 10
+}
+
+export default ClearCommand;
