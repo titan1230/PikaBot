431ceb92e683bd00e61ca0bc2a5316658a6bafe4
Akshat Jain <akshat00jain@gmail.com>
2024-01-15 16:48:42 +0530

    archive command

---
+++ src/slashCommands/archive.ts
@@ -0,0 +1,26 @@
+import { ChannelType, PermissionFlagsBits, SlashCommandBuilder, TextChannel } from "discord.js";
+import { SlashCommand } from "../types";
+
+const ClearCommand : SlashCommand = {
+    command: new SlashCommandBuilder()
+    .setName("archive")
+    .setDescription("archive")
+    .addChannelOption(option => {
+        return option
+        .setName("channel")
+        .setDescription("Channel to archive")
+        .setRequired(true)
+    })
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    execute: async interaction => {
+        const channel = interaction.options.getChannel("channel") as TextChannel
+
+        if(!channel) return interaction.reply({ content: "Channel not found", ephemeral: true });
+
+        channel.setParent(process.env.ARCHIVE_CATEGORY_ID!);
+        interaction.reply({ ephemeral: true, content: `Successfully archived ${channel}`});
+    },
+    cooldown: 10
+}
+
+export default ClearCommand;
