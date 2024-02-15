115a029dbc309571da740c6ad43b2d11c5d7a2f9
Akshat Jain <akshat00jain@gmail.com>
2024-02-15 16:15:08 +0530

    added channel id command

---
+++ src/slashCommands/channel_id.ts
@@ -0,0 +1,23 @@
+import { ChannelType, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../types";
+
+const ClearCommand : SlashCommand = {
+    command: new SlashCommandBuilder()
+    .setName("channel_id")
+    .setDescription("Send ID of a channel.")
+    .addChannelOption(option => {
+        return option
+        .setName("channel")
+        .setDescription("Channel to get ID of.")
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildText)
+    })
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    execute: interaction => {
+        const channel = interaction.options.getRole("channel", true);
+        interaction.reply(channel.id);
+    },
+    cooldown: 10
+}
+
+export default ClearCommand;
