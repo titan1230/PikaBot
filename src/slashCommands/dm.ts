ac098f0c3b5492bf9024c03e1d5eb8d4df5feb78
Akshat Jain <akshat00jain@gmail.com>
2024-02-15 16:07:53 +0530

    added embed system and logging

--- src/slashCommands/dm.ts
+++ src/slashCommands/dm.ts
@@ -1,4 +1,4 @@
-import { PermissionFlagsBits, SlashCommandBuilder, TextChannel } from "discord.js";
+import { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder, TextChannel } from "discord.js";
 import { SlashCommand } from "../types";
 
 const ClearCommand : SlashCommand = {
@@ -13,6 +13,13 @@ const ClearCommand : SlashCommand = {
     })
     .addStringOption(option => {
         return option
+        .setName("title")
+        .setDescription("Title of the message.")
+        .setRequired(true)
+        .setMaxLength(100)
+    })
+    .addStringOption(option => {
+        return option
         .setName("message")
         .setDescription("Message to send.")
         .setRequired(true)
@@ -20,24 +27,53 @@ const ClearCommand : SlashCommand = {
     })
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
     execute: async interaction => {
+
+        if (!interaction.inCachedGuild()) return;
+
         const user = interaction.options.getUser("user", true)
+        const title = interaction.options.getString("title", true)
         let messageID = interaction.options.getString("message", true)
+
+        const LOGGING_CHANNEL = interaction.guild?.channels.cache.get("721302707943637082") as TextChannel;
+
+        const embed = new EmbedBuilder()
+        .setTitle(title)
+        .setColor("Yellow")
+        .setThumbnail(interaction.client.user?.avatarURL() || null)
+        .setTimestamp()
 
         if (messageID.includes("-")) {
             messageID = messageID.split("-")[1];
         }
 
-        let msg;
+        let msg: any;
         try {
             msg = await interaction.channel?.messages.fetch(messageID);
+
+            if (msg?.content.length! > 1500) {
+                interaction.reply({ content: `Message content is too long to send in a DM.`, ephemeral: true })
+                return;
+            }
+            embed.setDescription(msg?.content!);
         } catch (err) {
             console.log(err)
             interaction.reply({ content: `Failed to fetch message.\nRe-check the message ID.`, ephemeral: true })
             return;
         }
 
-        await user.send(msg?.content!).then(() => {
+        await user.send({ embeds: [embed] }).then(() => {
             interaction.reply({ content: `Sent a DM to ${user.username}`, ephemeral: true })
+
+            const logEmbed = new EmbedBuilder()
+            .setTitle(`DM Sent to ${user.username}`)
+            .setColor("Random")
+            .setTimestamp()
+            .setDescription(`**Sent By - ${interaction.user.username}**\n\n**Message Content:**\n${msg?.content}`)
+            
+            LOGGING_CHANNEL.send({ embeds: [logEmbed] }).catch(() => {
+                console.log(`FAILED TO LOG - { DM SENT }`)
+            });
+            
         }).catch(() => {
             interaction.reply({ content: `Failed to send a DM to ${user.username}`, ephemeral: true })
         });
