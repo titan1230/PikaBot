ac44c8f3f5efc637fb8fe8029714855077e971dd
Akshat Jain <akshat00jain@gmail.com>
2024-02-17 18:43:08 +0530

    added new event for invite link detection

---
+++ src/events/antiInvite.ts
@@ -0,0 +1,45 @@
+import { EmbedBuilder, Message, PermissionFlagsBits, TextChannel } from "discord.js";
+import { BotEvent } from "../types";
+
+const event: BotEvent = {
+    name: "messageCreate",
+    execute: async (message: Message) => {
+        if (message.author.bot) return;
+        if (!message.guild) return;
+
+        const links = ["discord.gg", "discord.com/invite", "discordapp.com/invite"];
+
+        for (const link of links) {
+            if (!message.content.includes(link)) return;
+
+            const code = message.content.split(link)[1].split(" ")[0];
+
+            const isGuildCode = message.guild.invites.cache.find(invite => invite.code === code);
+            const isVanityCode = code === message.guild.vanityURLCode;
+
+            if (isGuildCode || isVanityCode) return;
+
+            const loggingChannel = await message.guild.channels.fetch(process.env.PIKA_LOGS) as TextChannel;
+
+            await message.delete().catch((err) => {});
+            
+            const emebd = new EmbedBuilder()
+                .setTitle("Anti-Invite")
+                .setDescription("You are not allowed to send invites here")
+                .setColor("Red")
+                .setTimestamp()
+                .setFooter({ text: message.author.tag, iconURL: message.author.displayAvatarURL() });
+            message.channel.send({ embeds: [emebd] });
+
+            const loggingEmbed = new EmbedBuilder()
+                .setTitle("Anti-Invite ❌")
+                .setDescription(`${message.author} has sent an invite in ${message.channel}\n\n**User ID:** ${message.author.id}\n\n**Invite:** ${link}${code}\n\n**Message:** ${message.content}`)
+                .setColor("Red")
+                .setTimestamp()
+                .setFooter({ text: message.author.tag, iconURL: message.author.displayAvatarURL() });
+            loggingChannel.send({ embeds: [loggingEmbed] })
+        }
+    }
+}
+
+export default event
