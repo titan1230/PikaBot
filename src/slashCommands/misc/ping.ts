3a54834320dc188c19fb6b95a8602c2f60e514d8
Akshat Jain <akshat00jain@gmail.com>
2024-02-20 22:35:12 +0530

    edited typo

--- src/slashCommands/misc/ping.ts
+++ src/slashCommands/misc/ping.ts
@@ -11,7 +11,7 @@ const command : SlashCommand = {
         interaction.reply({
             embeds: [
                 new EmbedBuilder()
-                .setAuthor({name: "MRC License"})
+                .setAuthor({name: "Pong!"})
                 .setDescription(`🏓 Pong! \n 📡 Ping: ${interaction.client.ws.ping}`)
                 .setColor(getThemeColor("text"))
             ]
